d1f4302988c20e0f5f48a819326f7be8ae2e5474
igor3007 <dok307@gmail.com>
2024-04-26 01:06:18 +0300

    save

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -201,6 +201,76 @@ document.addEventListener('DOMContentLoaded', function (event) {
         })
     }
 
+    /* ===============================================
+    slider team
+    ===============================================*/
+
+    if (document.querySelector('[data-slider="team"]')) {
+        const splideProject = new Splide('[data-slider="team"]', {
+
+            perPage: 4,
+            perMove: 1,
+            gap: 32,
+            pagination: false,
+
+            breakpoints: {
+                1024: {
+                    perPage: 3,
+                    gap: 20
+                },
+                768: {
+                    fixedWidth: 300,
+                    perPage: 1,
+                    pagination: true,
+                },
+
+
+
+            },
+
+            arrowPath: 'M13.531 8.523a1.835 1.835 0 012.567 0l10.37 10.213c.71.698.71 1.83 0 2.528l-10.37 10.213a1.835 1.835 0 01-2.566 0 1.768 1.768 0 010-2.528L22.618 20l-9.088-8.949a1.768 1.768 0 010-2.528z'
+        });
+
+        splideProject.mount();
+    }
+
+    /* ===============================================
+    slider project
+    ===============================================*/
+
+    if (document.querySelector('[data-slider="project"]')) {
+        const splideThumb = new Splide('[data-slider="project"]', {
+
+            perPage: 1,
+            perMove: 1,
+            gap: 24,
+            pagination: false,
+            isNavigation: false,
+            arrowPath: 'M13.531 8.523a1.835 1.835 0 012.567 0l10.37 10.213c.71.698.71 1.83 0 2.528l-10.37 10.213a1.835 1.835 0 01-2.566 0 1.768 1.768 0 010-2.528L22.618 20l-9.088-8.949a1.768 1.768 0 010-2.528z'
+        });
+
+        splideThumb.mount();
+    }
+
+    /* ===============================================
+    slider project-thumb 
+    ===============================================*/
+
+    if (document.querySelector('[data-slider="project-thumb"]')) {
+        var splide = new Splide('[data-slider="project-thumb"]', {
+
+            perPage: 1,
+            perMove: 1,
+            gap: 12,
+            pagination: false,
+            fixedWidth: 100,
+            isNavigation: false,
+            arrowPath: 'M13.531 8.523a1.835 1.835 0 012.567 0l10.37 10.213c.71.698.71 1.83 0 2.528l-10.37 10.213a1.835 1.835 0 01-2.566 0 1.768 1.768 0 010-2.528L22.618 20l-9.088-8.949a1.768 1.768 0 010-2.528z'
+        });
+
+        splide.mount();
+    }
+
 
 
 
