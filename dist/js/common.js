9aefcde21f82b7de0a83d19cc999408dee23838d
Igor <50610898+Igor3007@users.noreply.github.com>
2024-04-26 17:16:52 +0300

    save

--- dist/js/common.js
+++ dist/js/common.js
@@ -151,7 +151,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
         }
 
         mountPlayer(data) {
-            this.title.innerText = data.title
+            this.title.innerHTML = `<a href="${this.player.dataset.id}" target="_blank" >${data.title}</a>`
             this.desc.innerText = data.desc
             this.player.querySelector('img').src = data.preview;
             this.player.dataset.id = data.link
@@ -163,7 +163,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
         scrollTop() {
             window.scrollToTargetAdjusted({
                 elem: '.section-video-block',
-                offset: -120
+                offset: -170
             })
         }
 
@@ -238,18 +238,107 @@ document.addEventListener('DOMContentLoaded', function (event) {
     slider project
     ===============================================*/
 
+    class ProjectSlider {
+        constructor() {
+            this.sliderProject = null
+            this.thumbInstance = null
+        }
+
+        setSlider(slide) {
+            this.sliderProject = slide
+        }
+
+        setThumsSlider(el) {
+            this.thumbInstance = el
+            this.addEventsThumb()
+        }
+
+        addEventsThumb() {
+            this.thumbInstance.root.querySelectorAll('.splide__slide').forEach((item, index) => {
+                item.addEventListener('click', () => {
+                    this.thumbInstance.go(index)
+                })
+            })
+        }
+
+        getElProjectActiveSlide() {
+            return this.sliderProject.root.querySelector('.splide__slide.is-active')
+        }
+
+        changeImageOnProject(url) {
+            this.getElProjectActiveSlide().querySelector('[data-ps="main-image"]').setAttribute('src', url)
+        }
+
+        getTemplateSlide(url) {
+            return `
+                <div class="splide__slide">
+                    <picture> <img src="${url}" ></picture>
+                </div>
+            `;
+        }
+
+        createSlides(slide) {
+            let items = slide.querySelectorAll('.card-project__thumb li')
+
+            items.forEach(li => {
+                this.thumbInstance.add(this.getTemplateSlide(li.dataset.src))
+            })
+
+            this.addEventsThumb()
+        }
+
+        changeProjectActive(slide) {
+
+            if (!this.thumbInstance) return false
+
+            this.thumbInstance.remove((item) => true)
+            this.createSlides(slide)
+        }
+
+
+    }
+
+    window.PS = new ProjectSlider()
+
     if (document.querySelector('[data-slider="project"]')) {
-        const splideThumb = new Splide('[data-slider="project"]', {
+        const sliderWrp = document.querySelector('.project-thumb__slider')
+        const splideProjects = new Splide('[data-slider="project"]', {
 
             perPage: 1,
             perMove: 1,
-            gap: 24,
+            gap: 42,
             pagination: false,
-            isNavigation: false,
+            arrows: true,
+            speed: 800,
+
+            breakpoints: {
+
+                992: {
+                    fixedWidth: 360,
+                    perPage: 1,
+                    gap: 24,
+                    pagination: true
+                },
+
+                480: {
+                    fixedWidth: 305,
+                    gap: 12
+                },
+
+            },
+
             arrowPath: 'M13.531 8.523a1.835 1.835 0 012.567 0l10.37 10.213c.71.698.71 1.83 0 2.528l-10.37 10.213a1.835 1.835 0 01-2.566 0 1.768 1.768 0 010-2.528L22.618 20l-9.088-8.949a1.768 1.768 0 010-2.528z'
         });
 
-        splideThumb.mount();
+        splideProjects.on('ready', (e) => {
+            window.PS.setSlider(splideProjects)
+        })
+
+        splideProjects.on('active', (e) => {
+            window.PS.changeProjectActive(e.slide)
+        })
+
+        splideProjects.mount();
     }
 
     /* ===============================================
@@ -257,20 +346,66 @@ document.addEventListener('DOMContentLoaded', function (event) {
     ===============================================*/
 
     if (document.querySelector('[data-slider="project-thumb"]')) {
-        var splide = new Splide('[data-slider="project-thumb"]', {
+        const item = document.querySelector('[data-slider="project-thumb"]')
+        const sliderCarousel = new Splide(item, {
 
             perPage: 1,
             perMove: 1,
-            gap: 12,
+            gap: 8,
             pagination: false,
-            fixedWidth: 100,
-            isNavigation: false,
+            fixedWidth: 120,
+            updateOnMove: true,
+            arrows: false,
             arrowPath: 'M13.531 8.523a1.835 1.835 0 012.567 0l10.37 10.213c.71.698.71 1.83 0 2.528l-10.37 10.213a1.835 1.835 0 01-2.566 0 1.768 1.768 0 010-2.528L22.618 20l-9.088-8.949a1.768 1.768 0 010-2.528z'
         });
 
-        splide.mount();
+        const next = item.closest('.project-thumb').querySelector('[data-slider="project-thumb-next"]')
+        const prev = item.closest('.project-thumb').querySelector('[data-slider="project-thumb-prev"]')
+
+        const checkNavButton = () => {
+
+            let slidesWidth = 0
+            let containerWidth = sliderCarousel.root.querySelector('.splide__track').clientWidth
+
+            sliderCarousel.root.querySelectorAll('.splide__slide').forEach(item => {
+                slidesWidth += (item.clientWidth - 3) + sliderCarousel.options.gap
+            })
+
+            next.style.display = (slidesWidth <= containerWidth ? 'none' : 'flex')
+            prev.style.display = (slidesWidth <= containerWidth ? 'none' : 'flex')
+
+        }
+
+        sliderCarousel.on('ready', (e) => {
+            checkNavButton()
+            window.PS.setThumsSlider(sliderCarousel)
+        })
+
+        sliderCarousel.on('resized', (e) => {
+            checkNavButton()
+        })
+
+        sliderCarousel.on('moved', (e) => {
+            checkNavButton()
+        })
+
+        sliderCarousel.on('active', (e) => {
+            window.PS.changeImageOnProject(e.slide.querySelector('img').getAttribute('src'))
+        })
+
+        sliderCarousel.mount()
+
+        next.addEventListener('click', e => sliderCarousel.go('>'))
+        prev.addEventListener('click', e => sliderCarousel.go('<'))
+
+
     }
 
+
+    var path = document.querySelector('.main-block__image path');
+
+
+    console.log(path.getTotalLength())
 
 
 
